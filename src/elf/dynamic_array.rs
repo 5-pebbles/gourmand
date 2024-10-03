5a8f614e64e12470f184b5ac0a3af114a537f6e1
5-pebbles <5-pebble@protonmail.com>
2024-10-03 09:55:17 -0400

    feat(relocate): continue work on relocating the linker;

--- src/elf/dynamic_array.rs
+++ src/elf/dynamic_array.rs
@@ -1,6 +1,7 @@
 pub(crate) const DT_NULL: usize = 0;
 pub(crate) const DT_RELA: usize = 7;
 pub(crate) const DT_REL: usize = 17;
+pub(crate) const DT_TEXTREL: usize = 22;
 
 #[repr(C)]
 #[derive(Clone, Copy)]
--- src/linux/relocate.rs
+++ src/linux/relocate.rs
@@ -1,9 +1,15 @@
 use crate::{
+    elf::{
+        dynamic_array::{DynamicArrayItem, DynamicArrayIter, DT_RELA, DT_TEXTREL},
+        header::{self, ElfHeader},
+        program_header::{ElfProgramHeaderTable, PT_DYNAMIC, PT_LOAD},
+    },
     linux::auxiliary_iterator::{AuxiliaryIterator, AT_BASE, AT_ENTRY, AT_PAGE_SIZE},
     utils::no_std_debug_assert,
 };
 
 pub(crate) fn relocate_linker(auxiliary_iterator: AuxiliaryIterator) {
+    // TODO: This needs to be split up... real bad...
     no_std_debug_assert!(auxiliary_iterator
         .clone()
         .any(|value| value.kind == AT_PAGE_SIZE));
@@ -14,5 +20,36 @@ pub(crate) fn relocate_linker(auxiliary_iterator: AuxiliaryIterator) {
         .clone()
         .any(|value| value.kind == AT_ENTRY));
 
-    // todo!()
+    let (mut base, mut entry, mut page_size) = (0, 0, 0);
+
+    auxiliary_iterator.for_each(|value| match value.kind {
+        AT_BASE => base = value.value,
+        AT_ENTRY => entry = value.value,
+        AT_PAGE_SIZE => page_size = value.value,
+        _ => (),
+    });
+
+    let header = unsafe { &*(base as *const ElfHeader) };
+    no_std_debug_assert!(header.e_type == header::ET_DYN);
+
+    let program_header_table = ElfProgramHeaderTable::new(base, header.e_phoff, header.e_phnum);
+    let load_bias = base
+        - program_header_table
+            .iter()
+            .find(|f| f.p_type == PT_LOAD)
+            .unwrap()
+            .p_vaddr;
+
+    let dynamic_header = program_header_table
+        .into_iter()
+        .find(|h| h.p_type == PT_DYNAMIC)
+        .unwrap();
+
+    let dynamic_array = DynamicArrayIter::new(
+        dynamic_header.p_vaddr.wrapping_add(load_bias) as *const DynamicArrayItem
+    );
+
+    no_std_debug_assert!(dynamic_array.clone().any(|i| i.d_tag == DT_RELA));
+    // TODO: How do you handle these cases?
+    no_std_debug_assert!(!dynamic_array.clone().any(|i| i.d_tag == DT_TEXTREL))
 }
