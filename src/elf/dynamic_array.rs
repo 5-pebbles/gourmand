9ccf566e7694a6c6834a2d6117f456e328df321a
5-pebbles <5-pebble@protonmail.com>
2024-10-03 09:39:24 -0400

    feat(elf): add iterator over the dynamic array;

---
+++ src/elf/dynamic_array.rs
@@ -0,0 +1,36 @@
+pub(crate) const DT_NULL: usize = 0;
+pub(crate) const DT_RELA: usize = 7;
+pub(crate) const DT_REL: usize = 17;
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub(crate) struct DynamicArrayItem {
+    pub kind: usize,
+    pub value: usize,
+}
+
+#[derive(Clone, Copy)]
+pub(crate) struct DynamicArrayIter(*const DynamicArrayItem);
+
+impl DynamicArrayIter {
+    pub(crate) fn new(dynamic_array_pointer: *const DynamicArrayItem) -> Self {
+        Self(dynamic_array_pointer)
+    }
+
+    pub(crate) fn into_inner(self) -> *const DynamicArrayItem {
+        self.0
+    }
+}
+
+impl Iterator for DynamicArrayIter {
+    type Item = DynamicArrayItem;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let this = unsafe { *self.0 };
+        if this.kind == DT_NULL {
+            return None;
+        }
+        self.0 = unsafe { self.0.add(1) };
+        Some(this)
+    }
+}
--- src/elf/mod.rs
+++ src/elf/mod.rs
@@ -1,3 +1,4 @@
+pub(crate) mod dynamic_array;
 pub(crate) mod header;
 pub(crate) mod header_table;
 pub(crate) mod program_header;
