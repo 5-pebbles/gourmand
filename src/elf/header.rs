4e48ccb9a857259f776858603bd5e307d3fec5ec
5-pebbles <5-pebble@protonmail.com>
2024-10-03 09:18:13 -0400

    fix(elf): convert ElfHeader fields to specs names;

--- src/elf/header.rs
+++ src/elf/header.rs
@@ -1,18 +1,20 @@
+pub(crate) const ET_DYN: u16 = 3;
+
 #[repr(C)]
 #[derive(Clone, Copy, Default, PartialEq)]
 pub(crate) struct ElfHeader {
-    pub elf_identification: [u8; 16],
-    pub kind: u16,
-    pub instruction_set_architecture: u16,
-    pub elf_version: u32,
-    pub entry_point: usize,
-    pub program_header_table_offset: usize,
-    pub section_header_table_offset: usize,
-    pub processor_specific_flags: u32,
-    pub elf_header_size: u16,
-    pub program_header_table_entry_size: u16,
-    pub program_header_table_entry_count: u16,
-    pub section_header_table_entry_size: u16,
-    pub section_header_table_entry_count: u16,
-    pub section_header_table_entry_index_for_section_names: u16,
+    pub e_ident: [u8; 16],
+    pub e_type: u16,
+    pub e_machine: u16,
+    pub e_version: u32,
+    pub e_entry: usize,
+    pub e_phoff: usize,
+    pub e_shoff: usize,
+    pub e_flags: u32,
+    pub e_ehsize: u16,
+    pub e_phentsize: u16,
+    pub e_phnum: u16,
+    pub e_shentsize: u16,
+    pub e_shnum: u16,
+    pub e_shstrndx: u16,
 }
